5113394b8b9ace0d22965fee0858a7f046adafaa
Pranath Reddy <f20160572@hyderabad.bits-pilani.ac.in>
2020-04-27 15:38:11 +0530

    Update run_12ECG_classifier.py

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 import joblib
 from get_12ECG_features import get_12ECG_features
 from joblib import dump, load
+from tensorflow.keras.models import load_model
 
 def run_12ECG_classifier(data,header_data,classes,model):
 
@@ -12,9 +13,9 @@ def run_12ECG_classifier(data,header_data,classes,model):
     #current_score = np.zeros(num_classes)
 
     # Use your classifier here to obtain a label and score for each class.
-    features=np.asarray(get_12ECG_features(data,header_data))
-    preds = model.predict(features).toarray()
-    probas = model.predict_proba(features).toarray()
+    features=np.asarray(get_12ECG_features(data,header_data,model[1]))
+    preds = model[0].predict(features).toarray()
+    probas = model[0].predict_proba(features).toarray()
     
     pred_vals = np.mean(preds, axis=0)
     current_score = np.mean(probas, axis=0)
@@ -30,5 +31,6 @@ def load_12ECG_model():
 
     # load the model from disk
     loaded_model = load('classifier.joblib')
+    fe_model = load_model('./FE_Model.h5')
 
-    return loaded_model
+    return [loaded_model, fe_model]
